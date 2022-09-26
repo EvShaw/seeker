4c55d26fbb68faeeb807e5263988667fb935afdc
Evan Shaw <e85.shaw@gmail.com>
2022-09-26 16:10:17 -0700

    add new company feature

--- controllers/home.js
+++ controllers/home.js
@@ -2,4 +2,7 @@ module.exports = {
     getIndex: (req, res) => {
       res.render("index.ejs");
     },
+    newCompany: (req, res) => {
+      res.render("newCompany.ejs")
+    }
   };
--- routes/main.js
+++ routes/main.js
@@ -2,12 +2,12 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/auth");
 const homeController = require("../controllers/home");
-// const postsController = require("../controllers/posts");
 const companyController = require("../controllers/company")
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 //Main Routes - simplified for now
 router.get("/", homeController.getIndex);
+router.get("/newCompany",homeController.newCompany);
 
 router.get("/profile", ensureAuth, companyController.getProfile);
 
