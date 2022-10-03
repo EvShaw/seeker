80167f14624f8c40da66687d4cdea4462c36804a
Evan Shaw <e85.shaw@gmail.com>
2022-10-03 13:36:07 -0700

    add new company creation workflow

--- routes/company.js
+++ routes/company.js
@@ -7,27 +7,19 @@ const { ensureAuth, ensureGuest } = require("../middleware/auth");
 //Post Routes - simplified for now
 router.get("/:id", ensureAuth, companyController.getCompany);
 
+
 router.get("/addNewPosition/:id", companyController.addNewPosition);
 router.get("/addNewContact/:id", companyController.addNewContact);
 
 
-router.post("/createCompany", companyController.createCompany);
+
+router.post("/createNewCompany", companyController.createNewCompany);
 router.post("/createNewPosition/:id", companyController.createNewPosition);
 router.post("/createNewContact/:id", companyController.createNewContact);
 
 router.delete("/deleteCompany/:id", companyController.deleteCompany);
 router.delete("/deletePosition/:id", companyController.deletePosition);
-
-
-
-
-
-
-// router.post("/deletePost", upload.single("file"), postsController.createPost);
-
-// router.put("/likePost/:id", postsController.likePost);
-
-
+router.delete("/deleteContact/:id", companyController.deleteContact);
 
 
 module.exports = router;
